cb16a47fd7cf74af756c282b99dbee405320be98
Yuanhui <hyh618@gmail.com>
2022-12-16 16:29:52 +0800

    通讯完成

--- MiniChat.py
+++ MiniChat.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 import socket
+from time import ctime
+import json
 
 def signin():
-    server = socket.socket()
-    host = '192.168.1.14'
-    port = 8001
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 8000
+
+    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     
-    server.connect((host, port))
-    print('连接成功')
-    inData = '1283789478'
-    server.send(inData.encode('utf-8'))
-    print('发送成功！', inData)
-    server.close()
+    s.connect((host, port))
+    print('[{}]连接成功'.format(ctime()))
+    sendData = json.dumps((0, account.get(), password.get())) 
+    s.send(sendData.encode('utf-8'))
+    print('[{}]成功发送登录请求'.format(ctime()))
+    s.close()
 
 root = tk.Tk()
 root.title('Mini Chat')
