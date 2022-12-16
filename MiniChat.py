e7a43f16441df2d3d2c3400fb5b7f87ea0769642
Yuanhui <hyh618@gmail.com>
2022-12-16 17:00:38 +0800

    登录功能

--- MiniChat.py
+++ MiniChat.py
@@ -2,42 +2,57 @@ import tkinter as tk
 import socket
 from time import ctime
 import json
+import tkinter.messagebox as messagebox
 
-def signin():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostname()
-    port = 8000
+class MiniChat():
+    def prints(self, text):
+        print('[{}]{}'.format(ctime(), text))
 
-    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    
-    s.connect((host, port))
-    print('[{}]连接成功'.format(ctime()))
+    def signin(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = '192.168.1.14'
+        port = 8000
 
-    sendData = json.dumps((0, account.get(), password.get())) 
-    s.send(sendData.encode('utf-8'))
-    print('[{}]成功发送登录请求'.format(ctime()))
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        
+        s.connect((host, port))
+        self.prints('连接成功')
 
-    flag = s.recv(1024).decode('utf-8')
+        sendData = json.dumps((0, self.account.get(), self.password.get())) 
+        s.send(sendData.encode('utf-8'))
+        self.prints('成功发送登录请求')
 
-    s.close()
+        if (s.recv(1024).decode('utf-8') == '0'):
+            self.prints('登录成功')
+        else:
+            self.prints('登录失败，用户名或密码错误')
+            messagebox.showerror('错误', '用户名或密码错误')
 
-root = tk.Tk()
-root.title('Mini Chat')
+        s.close()
 
-tk.Label(root, text="账户：").grid(row=0)
-tk.Label(root, text="密码：").grid(row=1)
+    def sign_main(self):
+        root = tk.Tk()
+        root.title('Mini Chat')
 
-account = tk.Entry(root, width=20)
-account.grid(row=0, column=1)
+        tk.Label(root, text="账户：").grid(row=0)
+        tk.Label(root, text="密码：").grid(row=1)
 
-password = tk.Entry(root, width=20, show='*')
-password.grid(row=1, column=1)
+        self.account = tk.Entry(root, width=20)
+        self.account.grid(row=0, column=1)
 
-signin_button = tk.Button(root, text='登录', command=signin)
-signin_button.grid(row=2, column=0)
+        self.password = tk.Entry(root, width=20, show='*')
+        self.password.grid(row=1, column=1)
 
-signup_button = tk.Button(root, text='注册', state='active')
-signup_button.grid(row=2, column=1, sticky=tk.W)
+        signin_button = tk.Button(root, text='登录', command=self.signin)
+        signin_button.grid(row=2, column=0)
 
-root.mainloop()
+        signup_button = tk.Button(root, text='注册', state='active')
+        signup_button.grid(row=2, column=1, sticky=tk.W)
+
+        root.mainloop()
+
+    def __init__(self):
+        self.sign_main()
+
+MiniChat()
